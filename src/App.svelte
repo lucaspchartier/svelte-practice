937b4a1cf75bdc160aa54763fbc3c8bed321f97e
lucaspchartier <lucaspchartier@gmail.com>
2021-03-16 20:43:13 -0400

    chore(src/App.svelte): import HeroDetail to render  in App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+import HeroDetail from "./HeroDetail.svelte";
+
 	export let name;
 
 	let hero = {
@@ -10,7 +12,9 @@
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>>
+</main>
+
+<HeroDetail />
 
 <style>
 	main {
