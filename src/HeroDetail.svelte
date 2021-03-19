938fe1c8e015224551517bc5dc35024469ca5213
lucaspchartier <lucaspchartier@gmail.com>
2021-03-18 20:09:31 -0400

    chore(HeroDetail.svelte): add field for birth year in hero object

--- src/HeroDetail.svelte
+++ src/HeroDetail.svelte
@@ -9,4 +9,6 @@
 <div>
     <div><label for="name">Name</label>
     <input type="text" name="name" bind:value={hero.name}></div>
+    <div><label for="birthYear">Birth Year</label>
+    <input type="text" birthYear="birth year" bind:value={hero.birthYear}></div>
 </div>
