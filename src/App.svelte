3eb29a481babc08da0102124a0177ca09331dc8c
lucaspchartier <lucaspchartier@gmail.com>
2021-03-18 21:00:53 -0400

    chore(App.svelte): pass in heroes array to be rendered in imported HeroesList component

--- src/App.svelte
+++ src/App.svelte
@@ -17,7 +17,7 @@
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
-<HeroesList {hero} />
+<HeroesList heros={heroes} />
 <!-- <HeroDetail {hero} /> -->
 
 <style>
