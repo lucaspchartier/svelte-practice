c562f9a8c88e22e6c4f7790cae2a272682856900
lucaspchartier <lucaspchartier@gmail.com>
2021-03-18 20:37:14 -0400

    chore(src/HeroesList.svelte): add CSS styling under style tags in HeroesList component

--- src/HeroesList.svelte
+++ src/HeroesList.svelte
@@ -1,7 +1,26 @@
 <script>
-    
+
 </script>
 
 <style>
-
+    ul {
+        list-style-type: none;
+        padding: 4px;
+    }
+    ul > li {
+        padding: 4px;
+        cursor: pointer;
+    }
+    .selected {
+        background-color: yellow;
+    }
+    .row {
+        margin: 10px;
+        box-shadow: 0 0 5px gray;
+        padding: 10px 20px;
+    }
+    .error {
+        background: rgb(255, 219, 219);
+        padding: 10px 20px;
+    }
 </style>
