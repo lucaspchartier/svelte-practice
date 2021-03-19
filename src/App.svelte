65a63dd42d707ec75e458fd4fedf2c240a512b95
lucaspchartier <lucaspchartier@gmail.com>
2021-03-19 07:01:30 -0400

    fix(src/App.svelte): fix typo in heroes variable being passed in rendered HeroesList component

--- src/App.svelte
+++ src/App.svelte
@@ -17,7 +17,7 @@
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
-<HeroesList heros={heroes} />
+<HeroesList heroes={heroes} />
 <!-- <HeroDetail {hero} /> -->
 
 <style>
